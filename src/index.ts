f062196f60aad8972594283680fb6dd215b6f5ec
WatchDG <baranov@w3m.co>
2021-01-04 18:47:03 +0300

    add decorator instead `try catch` block

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ResultOk, ResultFail, ResultOK, ResultFAIL } from 'node-result';
+import { Result, ResultOk, tryCatchWrapperAsync } from 'node-result';
 import { HttpInstance } from 'http-instance';
 
 import { CarrierServiceId, CarrierService, CarrierServicesCreate } from './types/carrier_service';
@@ -14,363 +14,291 @@ export class Shopify {
   constructor(baseUrl: string, timeout = 1000) {
     this.instance = new HttpInstance({
       baseUrl,
-      timeout,
+      timeout
     });
   }
 
-  async getShop(): Promise<ResultOK<Shop> | ResultFAIL<Error>> {
-    try {
-      const {
-        data: { shop },
-      } = (await this.instance.get<{ shop: Shop }>('/admin/api/2020-10/shop.json')).unwrap();
-      return ResultOk(shop);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async getShop(): Promise<Result<Error, Shop>> {
+    type rT = { shop: Shop };
+    const url = '/admin/api/2020-10/shop.json';
+    const {
+      data: { shop }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(shop);
   }
 
-  async getCarrierServices(): Promise<ResultOK<CarrierService[]> | ResultFAIL<Error>> {
-    try {
-      const {
-        data: { carrier_services },
-      } = (
-        await this.instance.get<{ carrier_services: CarrierService[] }>('/admin/api/2020-10/carrier_services.json')
-      ).unwrap();
-      return ResultOk(carrier_services);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async getCarrierServices(): Promise<Result<Error, CarrierService[]>> {
+    type rT = { carrier_services: CarrierService[] };
+    const url = '/admin/api/2020-10/carrier_services.json';
+    const {
+      data: { carrier_services }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(carrier_services);
   }
 
-  async createCarrierService(
-    carrierServicesCreate: CarrierServicesCreate,
-  ): Promise<ResultOK<CarrierService> | ResultFAIL<Error>> {
-    try {
-      const payload = {
-        carrier_service: carrierServicesCreate,
-      };
-      const {
-        data: { carrier_service },
-      } = (
-        await this.instance.post<{ carrier_service: CarrierService }>(
-          '/admin/api/2020-10/carrier_services.json',
-          payload,
-        )
-      ).unwrap();
-      return ResultOk(carrier_service);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async createCarrierService(carrierServicesCreate: CarrierServicesCreate): Promise<Result<Error, CarrierService>> {
+    type rT = { carrier_service: CarrierService };
+    const url = '/admin/api/2020-10/carrier_services.json';
+    const payload = {
+      carrier_service: carrierServicesCreate
+    };
+    const {
+      data: { carrier_service }
+    } = (await this.instance.post<rT>(url, payload)).unwrap();
+    return ResultOk(carrier_service);
   }
 
-  async getCarrierService(id: CarrierServiceId): Promise<ResultOK<CarrierService> | ResultFAIL<Error>> {
-    try {
-      const {
-        data: { carrier_service },
-      } = (
-        await this.instance.get<{ carrier_service: CarrierService }>(`/admin/api/2020-10/carrier_services/${id}.json`)
-      ).unwrap();
-      return ResultOk(carrier_service);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async getCarrierService(id: CarrierServiceId): Promise<Result<Error, CarrierService>> {
+    type rT = { carrier_service: CarrierService };
+    const url = `/admin/api/2020-10/carrier_services/${id}.json`;
+    const {
+      data: { carrier_service }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(carrier_service);
   }
 
-  async deleteCarrierService(id: CarrierServiceId): Promise<ResultOK<null> | ResultFAIL<Error>> {
-    try {
-      (await this.instance.delete(`/admin/api/2020-10/carrier_services/${id}.json`)).unwrap();
-      return ResultOk(null);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async deleteCarrierService(id: CarrierServiceId): Promise<Result<Error, null>> {
+    const url = `/admin/api/2020-10/carrier_services/${id}.json`;
+    (await this.instance.delete(url)).unwrap();
+    return ResultOk(null);
   }
 
-  async getScriptTags(): Promise<ResultOK<ScriptTag[]> | ResultFAIL<Error>> {
-    try {
-      const {
-        data: { script_tags },
-      } = (await this.instance.get<{ script_tags: ScriptTag[] }>('/admin/api/2020-07/script_tags.json')).unwrap();
-      return ResultOk(script_tags);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async getScriptTags(): Promise<Result<Error, ScriptTag[]>> {
+    type rT = { script_tags: ScriptTag[] };
+    const url = '/admin/api/2020-07/script_tags.json';
+    const {
+      data: { script_tags }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(script_tags);
   }
 
-  async createScriptTag(scriptTagCreate: ScriptTagCreate): Promise<ResultOK<ScriptTag> | ResultFAIL<Error>> {
-    try {
-      const payload = {
-        script_tag: scriptTagCreate,
-      };
-      const {
-        data: { script_tag },
-      } = (
-        await this.instance.post<{ script_tag: ScriptTag }>('/admin/api/2020-07/script_tags.json', payload)
-      ).unwrap();
-      return ResultOk(script_tag);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async createScriptTag(scriptTagCreate: ScriptTagCreate): Promise<Result<Error, ScriptTag>> {
+    type rT = { script_tag: ScriptTag };
+    const url = '/admin/api/2020-07/script_tags.json';
+    const payload = {
+      script_tag: scriptTagCreate
+    };
+    const {
+      data: { script_tag }
+    } = (await this.instance.post<rT>(url, payload)).unwrap();
+    return ResultOk(script_tag);
   }
 
-  async getScriptTag(id: ScriptTagId): Promise<ResultOK<ScriptTag> | ResultFAIL<Error>> {
-    try {
-      const {
-        data: { script_tag },
-      } = (await this.instance.get<{ script_tag: ScriptTag }>(`/admin/api/2020-10/script_tags/${id}.json`)).unwrap();
-      return ResultOk(script_tag);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async getScriptTag(id: ScriptTagId): Promise<Result<Error, ScriptTag>> {
+    type rT = { script_tag: ScriptTag };
+    const url = `/admin/api/2020-10/script_tags/${id}.json`;
+    const {
+      data: { script_tag }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(script_tag);
   }
 
-  async deleteScriptTag(id: ScriptTagId): Promise<ResultOK<ScriptTag> | ResultFAIL<Error>> {
-    try {
-      (await this.instance.delete(`/admin/api/2020-10/script_tags/${id}.json`)).unwrap();
-      return ResultOk(null);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async deleteScriptTag(id: ScriptTagId): Promise<Result<Error, ScriptTag>> {
+    const url = `/admin/api/2020-10/script_tags/${id}.json`;
+    (await this.instance.delete(url)).unwrap();
+    return ResultOk(null);
   }
 
+  @tryCatchWrapperAsync
   async getWebHooks() {
-    try {
-      const {
-        data: { webhooks },
-      } = (await this.instance.get<{ webhooks: object }>('/admin/api/2020-10/webhooks.json')).unwrap();
-      return ResultOk(webhooks);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    type rT = { webhooks: object };
+    const url = '/admin/api/2020-10/webhooks.json';
+    const {
+      data: { webhooks }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(webhooks);
   }
 
+  @tryCatchWrapperAsync
   async createWebHook(webHookCreate: WebHookCreate) {
-    try {
-      const payload = {
-        webhook: webHookCreate,
-      };
-      const {
-        data: { webhook },
-      } = (await this.instance.post<{ webhook: object }>('/admin/api/2020-10/webhooks.json', payload)).unwrap();
-      return ResultOk(webhook);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    type rT = { webhook: object };
+    const url = '/admin/api/2020-10/webhooks.json';
+    const payload = {
+      webhook: webHookCreate
+    };
+    const {
+      data: { webhook }
+    } = (await this.instance.post<rT>(url, payload)).unwrap();
+    return ResultOk(webhook);
   }
 
+  @tryCatchWrapperAsync
   async getWebHook(id: WebHookId) {
-    try {
-      const {
-        data: { webhook },
-      } = (await this.instance.get<{ webhook: object }>(`/admin/api/2019-10/webhooks/${id}.json`)).unwrap();
-      return ResultOk(webhook);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    type rT = { webhook: object };
+    const url = `/admin/api/2019-10/webhooks/${id}.json`;
+    const {
+      data: { webhook }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(webhook);
   }
 
-  async deleteWebHook(id: WebHookId): Promise<ResultOK<null> | ResultFAIL<Error>> {
-    try {
-      (await this.instance.delete(`/admin/api/2020-10/webhooks/${id}.json`)).unwrap();
-      return ResultOk(null);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async deleteWebHook(id: WebHookId): Promise<Result<Error, null>> {
+    const url = `/admin/api/2020-10/webhooks/${id}.json`;
+    (await this.instance.delete(url)).unwrap();
+    return ResultOk(null);
   }
 
+  @tryCatchWrapperAsync
   async getOrders(options = {}) {
-    try {
-      const {
-        data: { orders },
-      } = (await this.instance.get<{ orders: object[] }>('/admin/api/2020-10/orders.json?status=any')).unwrap();
-      return ResultOk(orders);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    type rT = { orders: object[] };
+    const url = '/admin/api/2020-10/orders.json';
+    const {
+      data: { orders }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(orders);
   }
 
+  @tryCatchWrapperAsync
   async getOrder(id: OrderId) {
-    try {
-      const url = `/admin/api/2020-10/orders/${id}.json`;
-      const {
-        data: { order },
-      } = (await this.instance.get<{ order: object }>(url)).unwrap();
-      return ResultOk(order);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    type rT = { order: object };
+    const url = `/admin/api/2020-10/orders/${id}.json`;
+    const {
+      data: { order }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(order);
   }
 
+  @tryCatchWrapperAsync
   async updateOrder(id, updateData: UpdateOrder) {
-    try {
-      const order = Object.assign({ id }, updateData);
-      const payload = {
-        order,
-      };
-      const url = `/admin/api/2020-10/orders/${id}.json`;
-      (await this.instance.put(url, payload)).unwrap();
-      return ResultOk(null);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    const url = `/admin/api/2020-10/orders/${id}.json`;
+    const order = Object.assign({ id }, updateData);
+    const payload = {
+      order
+    };
+    (await this.instance.put(url, payload)).unwrap();
+    return ResultOk(null);
   }
 
+  @tryCatchWrapperAsync
   async deleteOrder(id: OrderId) {
-    try {
-      const url = `/admin/api/2020-10/orders/${id}.json`;
-      (await this.instance.delete(url)).unwrap();
-      return ResultOk(null);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    const url = `/admin/api/2020-10/orders/${id}.json`;
+    (await this.instance.delete(url)).unwrap();
+    return ResultOk(null);
   }
 
+  @tryCatchWrapperAsync
   async getOrderMetafields(id: OrderId) {
-    try {
-      const {
-        data: { metafields },
-      } = (await this.instance.get<{ metafields: object[] }>(`/admin/orders/${id}/metafields.json`)).unwrap();
-      return ResultOk(metafields);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    type rT = { metafields: object[] };
+    const url = `/admin/orders/${id}/metafields.json`;
+    const {
+      data: { metafields }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(metafields);
   }
 
+  @tryCatchWrapperAsync
   async getOrderMetafield(orderId: OrderId, metafieldId: OrderMetafieldId) {
-    try {
-      const {
-        data: { metafield },
-      } = (
-        await this.instance.get<{ metafield: object }>(`/admin/orders/${orderId}/metafields/${metafieldId}.json`)
-      ).unwrap();
-      return ResultOk(metafield);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    type rT = { metafield: object };
+    const url = `/admin/orders/${orderId}/metafields/${metafieldId}.json`;
+    const {
+      data: { metafield }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(metafield);
   }
 
+  @tryCatchWrapperAsync
   async updateOrderMetafield(orderId: OrderId, metafieldId: OrderMetafieldId, updateOrderMetafield: object) {
-    try {
-      const payload = {
-        metafield: Object.assign({ id: metafieldId }, updateOrderMetafield),
-      };
-      const {
-        data: { metafield },
-      } = (
-        await this.instance.put<{ metafield: object }>(
-          `/admin/orders/${orderId}/metafields/${metafieldId}.json`,
-          payload,
-        )
-      ).unwrap();
-      return ResultOk(metafield);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    type rT = { metafield: object };
+    const url = `/admin/orders/${orderId}/metafields/${metafieldId}.json`;
+    const payload = {
+      metafield: Object.assign({ id: metafieldId }, updateOrderMetafield)
+    };
+    const {
+      data: { metafield }
+    } = (await this.instance.put<rT>(url, payload)).unwrap();
+    return ResultOk(metafield);
   }
 
+  @tryCatchWrapperAsync
   async deleteOrderMetafield(orderId: OrderId, metafieldId: OrderMetafieldId) {
-    try {
-      (await this.instance.delete(`/admin/orders/${orderId}/metafields/${metafieldId}.json`)).unwrap();
-      return ResultOk(null);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    const url = `/admin/orders/${orderId}/metafields/${metafieldId}.json`;
+    (await this.instance.delete(url)).unwrap();
+    return ResultOk(null);
   }
 
-  async createCheckout(checkoutCreate: CheckoutCreate): Promise<ResultOK<Checkout> | ResultFAIL<Error>> {
-    try {
-      const payload = {
-        checkout: checkoutCreate,
-      };
-      const {
-        data: { checkout },
-      } = (await this.instance.post<{ checkout: Checkout }>('/admin/api/2020-10/checkouts.json', payload)).unwrap();
-      return ResultOk(checkout);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async createCheckout(checkoutCreate: CheckoutCreate): Promise<Result<Error, Checkout>> {
+    type rT = { checkout: Checkout };
+    const url = '/admin/api/2020-10/checkouts.json';
+    const payload = {
+      checkout: checkoutCreate
+    };
+    const {
+      data: { checkout }
+    } = (await this.instance.post<rT>(url, payload)).unwrap();
+    return ResultOk(checkout);
   }
 
-  async getCheckout(checkoutToken: CheckoutToken): Promise<ResultOK<Checkout> | ResultFAIL<Error>> {
-    try {
-      const {
-        data: { checkout },
-      } = (
-        await this.instance.get<{ checkout: Checkout }>(`/admin/api/2020-10/checkouts/${checkoutToken}.json`)
-      ).unwrap();
-      return ResultOk(checkout);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async getCheckout(checkoutToken: CheckoutToken): Promise<Result<Error, Checkout>> {
+    type rT = { checkout: Checkout };
+    const url = `/admin/api/2020-10/checkouts/${checkoutToken}.json`;
+    const {
+      data: { checkout }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(checkout);
   }
 
-  async getCheckoutShippingRates(
-    checkoutToken: CheckoutToken,
-  ): Promise<ResultOK<CheckoutShippingRates[]> | ResultFAIL<Error>> {
-    try {
-      const {
-        data: { shipping_rates },
-      } = (
-        await this.instance.get<{ shipping_rates: CheckoutShippingRates[] }>(
-          `/admin/api/2020-10/checkouts/${checkoutToken}/shipping_rates.json`,
-        )
-      ).unwrap();
-      return ResultOk(shipping_rates);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async getCheckoutShippingRates(checkoutToken: CheckoutToken): Promise<Result<Error, CheckoutShippingRates[]>> {
+    type rT = { shipping_rates: CheckoutShippingRates[] };
+    const url = `/admin/api/2020-10/checkouts/${checkoutToken}/shipping_rates.json`;
+    const {
+      data: { shipping_rates }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(shipping_rates);
   }
 
-  async updateCheckout(
-    checkoutToken: CheckoutToken,
-    checkoutUpdate: CheckoutUpdate,
-  ): Promise<ResultOK<Checkout> | ResultFAIL<Error>> {
-    try {
-      const payload = {
-        checkout: Object.assign(checkoutUpdate, { token: checkoutToken }),
-      };
-      const {
-        data: { checkout },
-      } = (
-        await this.instance.put<{ checkout: Checkout }>(`/admin/api/2020-10/checkouts/${checkoutToken}.json`, payload)
-      ).unwrap();
-      return ResultOk(checkout);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async updateCheckout(checkoutToken: CheckoutToken, checkoutUpdate: CheckoutUpdate): Promise<Result<Error, Checkout>> {
+    type rT = { checkout: Checkout };
+    const url = `/admin/api/2020-10/checkouts/${checkoutToken}.json`;
+    const payload = {
+      checkout: Object.assign(checkoutUpdate, { token: checkoutToken })
+    };
+    const {
+      data: { checkout }
+    } = (await this.instance.put<rT>(url, payload)).unwrap();
+    return ResultOk(checkout);
   }
 
-  async completeCheckout(checkoutToken: CheckoutToken): Promise<ResultOK<Checkout> | ResultFAIL<Error>> {
-    try {
-      const {
-        data: { checkout },
-      } = (
-        await this.instance.post<{ checkout: Checkout }>(`/admin/api/2020-10/checkouts/${checkoutToken}/complete.json`)
-      ).unwrap();
-      return ResultOk(checkout);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async completeCheckout(checkoutToken: CheckoutToken): Promise<Result<Error, Checkout>> {
+    type rT = { checkout: Checkout };
+    const url = `/admin/api/2020-10/checkouts/${checkoutToken}/complete.json`;
+    const {
+      data: { checkout }
+    } = (await this.instance.post<rT>(url)).unwrap();
+    return ResultOk(checkout);
   }
 
+  @tryCatchWrapperAsync
   async getProducts() {
-    try {
-      const {
-        data: { products },
-      } = (await this.instance.get<{ products: object[] }>('/admin/api/2020-10/products.json')).unwrap();
-      return ResultOk(products);
-    } catch (error) {
-      return ResultFail(error);
-    }
+    type rT = { products: object[] };
+    const url = '/admin/api/2020-10/products.json';
+    const {
+      data: { products }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(products);
   }
 
-  async getProductsCount(): Promise<ResultOK<number> | ResultFAIL<Error>> {
-    try {
-      const {
-        data: { count },
-      } = (await this.instance.get<{ count: number }>('/admin/api/2020-10/products/count.json')).unwrap();
-      return ResultOk(count);
-    } catch (error) {
-      return ResultFail(error);
-    }
+  @tryCatchWrapperAsync
+  async getProductsCount(): Promise<Result<Error, number>> {
+    type rT = { count: number };
+    const url = '/admin/api/2020-10/products/count.json';
+    const {
+      data: { count }
+    } = (await this.instance.get<rT>(url)).unwrap();
+    return ResultOk(count);
   }
 }
